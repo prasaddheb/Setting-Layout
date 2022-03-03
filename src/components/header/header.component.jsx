feacabb302b0f25d8e6892286f6e100f7b25638f
Prasad <dhebprasadshivaji@gmail.com>
2022-03-03 21:39:59 +0530

    done

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { ReactComponent as Setting} from '../../assets/setting.svg';
+import { ReactComponent as History} from '../../assets/history.svg'; 
+// import './header/styles.css';
+import '../header/header.styles.css';
+export default function Header(){
+
+
+    return(
+<div className='header'>
+<ul>
+  
+  <li>  <Setting className="logo"/></li>
+  <li>
+  <a href="/">General Setting</a>
+
+  </li> 
+  <li>
+  <History className="history"/ >
+  </li>
+  <li className="history" id="activity">Activity History</li>
+</ul>
+
+</div>
+    );
+}
---
+++ src/components/navbar/navbar.component.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { ReactComponent as Logo } from '../../assets/setting.svg';
+import { ReactComponent as Location} from '../../assets/location.svg';
+import { ReactComponent as Person} from '../../assets/person.svg';
+import { ReactComponent as Calender} from '../../assets/calender.svg';
+import { ReactComponent as Light} from '../../assets/light.svg';
+import { ReactComponent as Chart} from '../../assets/chart.svg';
+import './navbar.styles.css'; 
+export  default  function Navbar(){
+  
+  function Logoin(event) {
+    event.target.style.background = 'red';
+  }
+  function Logoout(event){
+    event.target.style.background="";
+  }
+return(
+    <div className='header'>
+      <ul>
+        <li>
+        <a href="/">client</a>
+      
+        </li> 
+        <li>  <Logo  onMouseOver={Logoin} onMouseOut={Logoout} className="logo"/></li>
+        <li>  <Location  onMouseOver={Logoin} onMouseOut={Logoout} className="logo"/></li>
+        <li>  <Person   onMouseOver={Logoin} onMouseOut={Logoout}className="logo"/></li>
+        <li>  <Calender  onMouseOver={Logoin} onMouseOut={Logoout}className="logo"/></li>
+        <li>  <Light   onMouseOver={Logoin} onMouseOut={Logoout}className="logo"/></li>
+        <li>  <Chart  onMouseOver={Logoin} onMouseOut={Logoout} className="logo"/></li>
+     </ul>
+     </div>
+);
+}
+// export default Header;
